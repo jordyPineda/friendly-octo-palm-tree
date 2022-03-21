564a2b3acfa588a000e992c03fb53bcd2cbd8ea4
jordy Pineda <jepinedav@gmail.com>
2022-03-21 17:06:01 -0500

    4

--- src/componentes/Tasks.js
+++ src/componentes/Tasks.js
@@ -1,15 +1,10 @@
 import React,{Component} from "react";
+import Task from './Task.js';
 
 class Tasks extends Component{
 
     render(){
-        return this.props.task.map( e => <p hey={e.id}>
-            {e.title} - {e.descripcion} - {e.done} - {e.id}
-            <input type='checkbox' />
-            <button>
-                x
-            </button>
-        </p>)
+        return this.props.task.map( e => <Task task={e} key={e.id}/>)
     }
 }
 
--- src/componentes/task.js
+++ src/componentes/task.js
@@ -1,3 +1,26 @@
 import React,{Component} from "react";
 
 
+class Task extends Component{
+    render(){
+
+        //tener en cuenta
+        const {task} = this.props;
+
+        return <div>
+
+                    {this.props.task.title} -
+                    {this.props.task.descripcion} - 
+                    {this.props.task.done} - 
+                    {this.props.task.id}
+                    <input type='checkbox' />
+                    <button>
+                        x
+                    </button>
+            
+                </div>
+        
+    }
+}
+
+export default Task;
